2a7ac40de2b35dbb160776473c2852074fa0550b
Steve Barnett <steve@naga.co.za>
2015-10-07 06:24:44 +0200

    Use sprite smith, unsuccessfully, manually

---
+++ .spritesmith.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var util = require('util');
+
+module.exports = {
+  src: './css/img/rape-crisis/*.{png,gif,jpg}',
+  destImage: './css/img/rape-crisis/icons.png',
+  destCSS: './css/rape-crisis-icons.css',
+  padding: 2,
+  algorithm: 'top-down',
+  algorithmOpts: { sort: false },
+  engine: 'gmsmith',
+  cssOpts: {
+    cssClass: function (item) {
+      return util.format('.i-rape-crisis-%s:before', item.name);
+    }
+  }
+};
