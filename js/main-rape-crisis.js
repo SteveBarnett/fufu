8e174bbc553a0a0d5162d090c5a9591e7a69c750
Steve Barnett <steve@naga.co.za>
2015-11-06 08:53:15 +0200

    Add pushState tab stuff

--- js/main-rape-crisis.js
+++ js/main-rape-crisis.js
@@ -66,40 +66,56 @@ var fufu = (function () {
   var docEl,
   listTabs,
   listTabItems,
+  numListTabItems,
+  targetListTabItem,
   currentHash,
   numListTabs,
   targetTab,
   targetTabList,
   targetTabItem,
   targetTabItems,
-  numTargetTabItems;
+  numTargetTabItems,
+  targetTabId,
+  hasPushState;
 
   // tabs
 
   // grab all the elements we need
   docEl  = document.documentElement;
   listTabs = docEl.querySelectorAll('.list-tabs');
+  listTabItems = docEl.querySelectorAll('.list-tab-item');
 
   // mark the doc as js-ready, having cut the mustard
   fufu.removeClass(docEl, 'no-js');
   fufu.addClass(docEl, 'js');
 
-  // show a tab if it's in the URL
+  // js version of show / hide tabs, for browsers with pushState
+  hasPushState = ('pushState' in history);
+  if(hasPushState) {
+    fufu.addClass(docEl, 'pushState');
+  }
+
+  // show a tab and tab item, if a hash is in the URL
   currentHash = window.location.hash;
-  targetTabItem = docEl.querySelector('[href="' + currentHash + '"]');
-  // if there's a tab hash in the URL, select that tab
   if (currentHash.length > 0 && targetTabItem !== 'null') {
+    targetTabItem = docEl.querySelector('[href="' + currentHash + '"]');
+    targetListTabItem =  docEl.querySelector(currentHash);
     fufu.addClass(targetTabItem.parentNode, 'tab-selected');
+    if (hasPushState) {
+      fufu.addClass(targetListTabItem, 'list-tab-item-selected');
+    }
   } else {
   // otherwise select the first one
     fufu.addClass(listTabs[0].querySelector('li'), 'tab-selected');
+    if (hasPushState) {
+      fufu.addClass(listTabItems[0], 'list-tab-item-selected');
+    }
   }
 
   // listen for clicks on the listTabs
   numListTabs = listTabs.length;
   for (var i = 0; i < numListTabs; i++) {
     listTabs[i].addEventListener('click', function(ev){
-
       targetTab = fufu.getTarget(ev).parentNode;
       targetTabList = targetTab.parentNode;
       targetTabItems = targetTabList.querySelectorAll('li');
@@ -109,6 +125,22 @@ var fufu = (function () {
         fufu.removeClass(targetTabItems[j], 'tab-selected');
       }
       fufu.addClass(targetTab, 'tab-selected');
+
+      // to prevent scrolling on clicking
+      if(hasPushState) {
+        ev.preventDefault();
+
+        targetListTabItem = docEl.querySelector(fufu.getTarget(ev).getAttribute('href'));
+
+        history.pushState({}, '', fufu.getTarget(ev).href);
+
+        numListTabItems = listTabItems.length;
+        for (var l = 0; l < numListTabItems; l++) {
+          fufu.removeClass(listTabItems[l],  'list-tab-item-selected');
+        }
+
+        fufu.addClass(targetListTabItem, 'list-tab-item-selected');
+      }
     });
   }
 
