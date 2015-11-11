0fd57a3edd80cb91c4c9b5fbcf3ebe2c7630e5dc
Steve Barnett <steve@naga.co.za>
2015-11-11 09:45:40 +0200

    Update spritesmith

--- .spritesmith.js
+++ .spritesmith.js
@@ -4,7 +4,7 @@ var util = require('util');
 
 module.exports = {
   src: './css/img/rape-crisis/icons/*.{png,gif,jpg}',
-  destImage: './css/img/rape-crisis/icons/icons.png',
+  destImage: './css/img/rape-crisis/icons.png',
   destCSS: './css/rape-crisis-icons.css',
   padding: 0,
   algorithm: 'top-down',
@@ -12,7 +12,7 @@ module.exports = {
   engine: 'gmsmith',
   cssOpts: {
     cssClass: function (item) {
-      return util.format('.i-rape-crisis-%s:before', item.name);
+      return util.format('.i-rape-crisis-%s', item.name);
     }
   }
 };
